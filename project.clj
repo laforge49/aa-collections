ff90994da1d0c57dcf7274069b3c0e698a5f6fac
Bill La Forge <laforge49@gmail.com>
2015-08-10 09:16:01 -0400

    Updated project cli

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject aa-collections "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure library for AA-Trees"
+  :url "https://github.com/laforge49/aa-collections"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
