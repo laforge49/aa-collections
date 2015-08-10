435ccb7e181876f783f503bbeb399df3cd0d8b37
Bill La Forge <laforge49@gmail.com>
2015-08-10 13:17:24 -0400

    require & import!

---
+++ src/aa_collections/immutable_set.clj
@@ -0,0 +1,6 @@
+(ns aa-collections.immutable-set
+  (:import clojure.lang.IPersistentSet))
+
+(deftype AAImmutableSet []
+  ;IPersistentSet
+  )
--- src/aa_collections/immutableset.clj
+++
@@ -1,8 +0,0 @@
-(ns aa-collections.immutableset
-  (:import clojure.lang.IPersistentSet))
-
-(deftype aa-immutable-set []
-  ;IPersistentSet
-  )
-
-(defn new-aa-immutable-set [] (aa-immutable-set.))
