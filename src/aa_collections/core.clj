7bfffff05cd6ff6dc9ebe6d7ead2838ec76a7ed8
Bill La Forge <laforge49@gmail.com>
2015-08-10 11:49:23 -0400

    stubbed a first deftype

--- src/aa_collections/core.clj
+++
@@ -1,6 +0,0 @@
-(ns aa-collections.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
---
+++ test/aa_collections/immutable_set_test.clj
@@ -0,0 +1,5 @@
+(ns aa-collections.immutable-set-test
+  (:require [clojure.test :refer :all]
+            [aa-collections.immutableset :refer :all]))
+
+(println (new-aa-immutable-set))
