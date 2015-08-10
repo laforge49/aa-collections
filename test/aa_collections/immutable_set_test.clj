b089f0a07a285d7f3ed03ec98c3914be71be75bf
Bill La Forge <laforge49@gmail.com>
2015-08-10 13:22:59 -0400

    -> and drop import

--- test/aa_collections/immutable_set_test.clj
+++ test/aa_collections/immutable_set_test.clj
@@ -1,6 +1,6 @@
 (ns aa-collections.immutable-set-test
   (:require [clojure.test :refer :all]
             [aa-collections.immutable-set :refer :all])
-  (:import (aa_collections.immutable_set AAImmutableSet)))
+  )
 
-(println (AAImmutableSet.))
+(println (->AAImmutableSet))
