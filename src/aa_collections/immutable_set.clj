4be0e5fd9e4ca9ab238541d73f6d5228d86e7201
Bill La Forge <laforge49@gmail.com>
2015-08-11 17:31:55 -0400

    empty

--- src/aa_collections/immutable_set.clj
+++ src/aa_collections/immutable_set.clj
@@ -1,32 +1,26 @@
 (ns aa-collections.immutable-set
   (:import (clojure.lang RT)))
 
-(defn isNil [x] (or (nil? x) (zero? (.-level x))))
-
-(defprotocol AASetInternal
-  (nixnay [this])
-  )
+(defn isNada [x] (or (nil? x) (zero? (.-level x))))
 
 (deftype AASet [level left right value comparator nada]
-  AASetInternal
-  (nixnay [this]
-    (if (isNil this)
-      this
-      nada))
   clojure.lang.IPersistentSet
   (seq [_] nil)
   (count [_] 0)
   (cons [_ o] nil)
-  (empty [_] nil)
-  (equiv [_ o] false)
-  (disjoin [_ key] nil)
-  (contains [_ key] false)
-  (get [_ key] nil)
+  (empty [this]
+    (if (isNada this)
+      this
+      nada))
+    (equiv [_ o] false)
+    (disjoin [_ key] nil)
+    (contains [_ key] false)
+    (get [_ key] nil)
 
-  clojure.lang.Reversible
-  (rseq [_] nil)
-  )
+    clojure.lang.Reversible
+    (rseq [_] nil)
+    )
 
-(defn aa-empty-set
-  ([] (aa-empty-set RT/DEFAULT_COMPARATOR))
-  ([comparator] (->AASet 0 nil nil nil comparator nil)))
+  (defn aa-empty-set
+    ([] (aa-empty-set RT/DEFAULT_COMPARATOR))
+    ([comparator] (->AASet 0 nil nil nil comparator nil)))
