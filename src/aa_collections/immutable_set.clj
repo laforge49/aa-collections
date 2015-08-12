185d15c2793722220fcb75b783fa3c0b8a422f5f
Bill La Forge <laforge49@gmail.com>
2015-08-11 22:21:32 -0400

    skew

--- src/aa_collections/immutable_set.clj
+++ src/aa_collections/immutable_set.clj
@@ -1,26 +1,60 @@
 (ns aa-collections.immutable-set
   (:import (clojure.lang RT)))
 
-(defn isNada [x] (or (nil? x) (zero? (.-level x))))
+(defn nada? [x] (or (nil? x) (zero? (.-level x))))
+
+(defprotocol AASetInternal
+  (skew [this])
+  )
+
+(declare iskew)
 
 (deftype AASet [level left right value comparator nada]
+  AASetInternal
+  (skew [this] (iskew this))
+
   clojure.lang.IPersistentSet
   (seq [_] nil)
   (count [_] 0)
   (cons [_ o] nil)
   (empty [this]
-    (if (isNada this)
+    (if (zero? level)
       this
       nada))
-    (equiv [_ o] false)
-    (disjoin [_ key] nil)
-    (contains [_ key] false)
-    (get [_ key] nil)
+  (equiv [_ o] false)
+  (disjoin [_ key] nil)
+  (contains [_ key] false)
+  (get [_ key] nil)
 
-    clojure.lang.Reversible
-    (rseq [_] nil)
-    )
+  clojure.lang.Reversible
+  (rseq [_] nil)
+  )
 
-  (defn aa-empty-set
-    ([] (aa-empty-set RT/DEFAULT_COMPARATOR))
-    ([comparator] (->AASet 0 nil nil nil comparator nil)))
+(defn aa-empty-set
+  ([] (aa-empty-set RT/DEFAULT_COMPARATOR))
+  ([comparator] (->AASet 0 nil nil nil comparator nil)))
+
+(defn- iskew
+  [this]
+  (cond
+    (nada? this)
+    this
+    (nada? (.-left this))
+    this
+    (= (.-level (.-left this)) (.-level this))
+    (let [l (.-left this)]
+      (->AASet
+        (.-level l)
+        (.-left l)
+        (->AASet
+          (.-level this)
+          (.-right l)
+          (.-right this)
+          (.-value this)
+          (.-comparator this)
+          (.-nada this))
+        (.-value l)
+        (.-comparator l)
+        (.-nada l)))
+    :else
+    this))
