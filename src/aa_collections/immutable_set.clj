11227e33ed86b9c0b5d26c59ed0df4e048c72d47
Bill La Forge <laforge49@gmail.com>
2015-08-14 17:26:49 -0400

    sfirst snext

--- src/aa_collections/immutable_set.clj
+++ src/aa_collections/immutable_set.clj
@@ -1,27 +1,32 @@
 (ns aa-collections.immutable-set
-  (:import (clojure.lang RT)))
+  (:import (clojure.lang RT Counted)))
 
-(defprotocol AASetNodeInternal
+(defprotocol IAASetNode
+  (sfirst [this])
+  (snext [this x])
   (left-node [this])
   (right-node [this])
-  (new-node [this value level left right])
+  (new-node [this value level left right cnt])
   (revise [this & args])
   (skew [this])
   (split [this])
   (insert [this x])
+  (emty [this])
+  (to-str [this])
   )
 
 (declare ->AASetNode)
 
-(defn nada? [x] (or (nil? x) (zero? (.-level x))))
+(defn nada? [x]
+  (or (nil? x) (zero? (.-level x))))
 
 (defn aa-empty-set-node
   ([] (aa-empty-set-node RT/DEFAULT_COMPARATOR))
-  ([comparator] (->AASetNode nil 0 nil nil comparator nil)))
+  ([comparator] (->AASetNode nil 0 nil nil 0 comparator nil)))
 
 (defn- inew-node
-  [this value level left right]
-  (->AASetNode value level left right (.-comparator this) (empty this)))
+  [this value level left right cnt]
+  (->AASetNode value level left right cnt (.-comparator this) (emty this)))
 
 (defn- irevise
   [this & args]
@@ -31,6 +36,7 @@
       (get m :level (.-level this))
       (get m :left (.-left this))
       (get m :right (.-right this))
+      (get m :cnt (.-cnt this))
       (.-comparator this)
       (.-nada this))))
 
@@ -43,7 +49,9 @@
     this
     (= (.-level (.-left this)) (.-level this))
     (let [l (.-left this)]
-      (.revise l :right (.revise this :left (.-right l))))
+      (irevise l
+               :right (irevise this :left (.-right l) :cnt (- (.-cnt this) (.-cnt l)))
+               :cnt (.-cnt this)))
     :else
     this))
 
@@ -56,46 +64,81 @@
     this
     (= (.-level this) (.-level (.-right (.-right this))))
     (let [r (.-right this)]
-      (.revise r :level (+ (.-level r) 1) :left (.revise this :right (.-left r))))
+      (irevise r
+               :level (+ (.-level r) 1)
+               :left (irevise this :right (.-left r) :cnt (- (.-cnt this) (.-cnt r)))
+               :cnt (.-cnt this)))
     :else
     this))
 
 (defn- iinsert
   [this x]
   (if (nada? this)
-    (new-node this x 1 nil nil)
+    (new-node this x 1 nil nil 1)
     (let [c (.compare (.-comparator this) x (.-value this))]
-      (.split (.skeq (cond
-        (< c 0)
-        (.revise this :left (iinsert (left-node this) x))
-        (> c 0)
-        (.revise this :right (iinsert (right-node this) x)))))))
+      (isplit (iskew (cond
+                       (< c 0)
+                       (let [l (iinsert (left-node this) x)
+                             d (- (.-cnt l) (.cnt (left-node this)))
+                             c (+ (.cnt this) d)]
+                         (irevise this :left l :cnt c))
+                       (> c 0)
+                       (let [r (insert (right-node this) x)
+                             d (- (.-cnt r) (.cnt (right-node this)))
+                             c (+ (.cnt this) d)]
+                         (irevise this :right r :cnt c)))))))
   )
 
-(deftype AASetNode [value level left right comparator nada]
-  AASetNodeInternal
+(deftype AASetNode [value level left right cnt comparator nada]
+  IAASetNode
+  (sfirst [this]
+    (cond
+      (nada? this) nil
+      (nada? left) value
+      :else (sfirst left)))
+  (snext [this x]
+    (if (nada? this)
+      nil
+      (let [c (.compare comparator x value)]
+        (cond
+          (zero? c) (sfirst (right-node this))
+          (> c 0) (snext (right-node this) x)
+          :else (let [n (snext (left-node this) x)]
+                  (if (nil? n)
+                    value
+                    n))))))
   (left-node [this]
     (if (nada? left)
-      (empty this)
+      (emty this)
       left))
   (right-node [this]
     (if (nada? right)
-      (empty this)
+      (emty this)
       right))
-  (new-node [this val lvl l r] (inew-node this val lvl l r))
+  (new-node [this val lvl l r c] (inew-node this val lvl l r c))
   (revise [this & args] (irevise this args))
   (skew [this] (iskew this))
   (split [this] (isplit this))
   (insert [this x] (iinsert this x))
+  (emty [this]
+    (if (nada? this)
+      this
+      (.-nada this)))
+  (to-str [this]
+    (if (nada? this)
+      ""
+      (str (to-str (left-node this)) " " value " " (to-str (right-node this)))))
 
+  Counted
+  (count [this] cnt)
+  )
+
+(deftype AASet [comparator nada]
   clojure.lang.IPersistentSet
   (seq [_] nil)
   (count [_] 0)
   (cons [this x] nil)
-  (empty [this]
-    (if (zero? level)
-      this
-      nada))
+  (empty [this] nil)
   (equiv [_ o] false)
   (disjoin [_ key] nil)
   (contains [_ key] false)
@@ -103,4 +146,6 @@
 
   clojure.lang.Reversible
   (rseq [_] nil)
+
+  Counted
   )
