2eabd1e788ff3f0ec0f42aa85121765cfe51871b
Bill La Forge <laforge49@gmail.com>
2015-08-21 11:22:59 -0400

    distinguish fields

--- src/aa_collections/AASetSequence.clj
+++ src/aa_collections/AASetSequence.clj
@@ -21,26 +21,26 @@
 (defn -init
   ([iter]
    (let [s (->seq-state iter (atom nil) (atom nil))]
-     (reset! (.val s) s)
-     (reset! (.rst s) s)
+     (reset! (.-val s) s)
+     (reset! (.-rst s) s)
      [[] s]))
   ([meta s]
    [[meta] s])
   )
 
-(defn -withMeta [this meta] (new AASetSequence meta (.state this)))
+(defn -withMeta [this meta] (new AASetSequence meta (.-state this)))
 
 (defn -first [this]
-  (let [s (.state this)
-        v (.val s)]
+  (let [s (.-state this)
+        v (.-val s)]
     (if (= s @v)
-      (swap! v #(if (= s %) (.next (.iter s)))))
+      (swap! v #(if (= s %) (.next (.-iter s)))))
     @(.-val s)))
 
 (defn -next [this]
-  (let [s (.state this)
-        r (.rst s)]
+  (let [s (.-state this)
+        r (.-rst s)]
     (when (= s @r)
       (-first this)
-      (swap! r #(if (= s %) (-create (.iter s)))))
+      (swap! r #(if (= s %) (-create (.-iter s)))))
     @(.-rst s)))
