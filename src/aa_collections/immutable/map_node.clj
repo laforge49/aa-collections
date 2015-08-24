48afe1b4078a4a3a68182201a123d7f83840befd
Bill La Forge <laforge49@gmail.com>
2015-08-24 17:05:46 -0400

    insert

--- src/aa_collections/immutable/map_node.clj
+++ src/aa_collections/immutable/map_node.clj
@@ -10,7 +10,8 @@
   (new-node [this t2 level left right cnt])
   (revise [this & args])
   (skew [this])
-  (split [this]))
+  (split [this])
+  (insert [this t2]))
 
 (declare ->MapNode)
 
@@ -60,8 +61,8 @@
           c (+ 1 (.cntr l) (.cntr r))]
       (if (or (not= v t2)
               (not= lev level)
-              (not= l (left-node this))
-              (not= r (right-node this)))
+              (not= l (.left-node this))
+              (not= r (.right-node this)))
         (.new-node this v lev l r c)
         this)))
 
@@ -90,4 +91,18 @@
                :left (.revise this :right (.-left right)))
       :else
       this))
+
+  (insert [this t2]
+    (if (emty? this)
+      (.new-node this t2 1 nil nil 1)
+      (let [c (.cmpr this t2)]
+        (.split (.skew (cond
+                         (< c 0)
+                         (let [oldl (.left-node this)
+                               l (.insert oldl t2)]
+                           (.revise this :left l))
+                         (> c 0)
+                         (let [oldr (.right-node this)
+                               r (.insert oldr t2)]
+                           (.revise this :right r))))))))
   )
