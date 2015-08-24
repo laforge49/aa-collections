412251fdc64a6a36fb584af40a8718c334a1c7b5
Bill La Forge <laforge49@gmail.com>
2015-08-24 15:12:20 -0400

    some IMapNode

--- src/aa_collections/immutable/map_node.clj
+++ src/aa_collections/immutable/map_node.clj
@@ -1,38 +1,93 @@
 (ns aa-collections.immutable.map-node
   (:import (clojure.lang RT)))
 
-(deftype MapNode [t2 level left right cnt comparator nada])
+(defprotocol IMapNode
+  (emty [this])
+  (cmpr [this x])
+  (cntr [this])
+  (right-node [this])
+  (left-node [this])
+  (new-node [this t2 level left right cnt])
+  (revise [this & args])
+  (skew [this])
+  (split [this]))
+
+(declare ->MapNode)
 
 (defn emty? [x]
   (or (nil? x) (zero? (.-level x))))
-
-(defn emty [this]
-  (if (emty? this)
-    this
-    (.-nada this)))
-
-(defn cmpr [this x]
-  (.compare (.-comparator this) x (.getKey (.-t2 this))))
-
-(defn cntr [this]
-  (if (emty? this)
-    0
-    (.-cnt this)))
-
-(defn right-node [this]
-  (if (emty? (.-right this))
-    (emty this)
-    (.-right this)))
-
-(defn- left-node [this]
-  (if (emty? (.-left this))
-    (emty this)
-    (.-left this)))
 
 (defn emty-node
   ([] (emty-node RT/DEFAULT_COMPARATOR))
   ([comparator] (->MapNode nil 0 nil nil 0 comparator nil)))
 
-(defn new-node
-  [this t2 level left right cnt]
-  (->MapNode t2 level left right cnt (.-comparator this) (emty this)))
+(deftype MapNode [t2 level left right cnt comparator nada]
+
+  IMapNode
+
+  (emty [this]
+    (if (emty? this)
+      this
+      nada))
+
+  (cmpr [this x]
+    (.compare comparator x (.getKey (t2 this))))
+
+  (cntr [this]
+    (if (emty? this)
+      0
+      cnt))
+
+  (right-node [this]
+    (if (emty? right)
+      (.emty this)
+      right))
+
+  (left-node [this]
+    (if (emty? (.-left this))
+      (emty this)
+      (.-left this)))
+
+  (new-node [this t2 level left right cnt]
+    (->MapNode t2 level left right cnt (.-comparator this) (.emty this)))
+
+  (revise [this & args]
+    (let [m (apply array-map args)
+          v (get m :t2 t2)
+          lev (get m :level level)
+          l (get m :left (.left-node this))
+          r (get m :right (.right-node this))
+          c (+ 1 (.cntr l) (.cntr r))]
+      (if (or (not= v t2)
+              (not= lev level)
+              (not= l (left-node this))
+              (not= r (right-node this)))
+        (.new-node this v lev l r c)
+        this)))
+
+  (skew
+    [this]
+    (cond
+      (emty? this)
+      this
+      (emty? left)
+      this
+      (= (.-level left) level)
+      (let [l left]
+        (.revise l :right (.revise this :left (.-right l))))
+      :else
+      this))
+
+  (split [this]
+    (cond
+      (emty? this)
+      this
+      (or (emty? right) (emty? (.-right right)))
+      this
+      (= level (.-level (.-right right)))
+      (.revise right
+               :level (+ 1 (.-level right))
+               :left (.revise this :right (.-left right)))
+      :else
+      this))
+  )
