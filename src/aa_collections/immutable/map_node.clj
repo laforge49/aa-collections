541b978556a87d7415890cdc16781cfd251f6c26
Bill La Forge <laforge49@gmail.com>
2015-08-24 13:54:47 -0400

    started map-node

---
+++ src/aa_collections/immutable/map_node.clj
@@ -0,0 +1,34 @@
+(ns aa-collections.immutable.map-node
+  (:import (clojure.lang RT)))
+
+(deftype MapNode [t2 level left right cnt comparator nada])
+
+(defn emty? [x]
+  (or (nil? x) (zero? (.-level x))))
+
+(defn emty-node
+  ([] (emty-node RT/DEFAULT_COMPARATOR))
+  ([comparator] (->MapNode nil 0 nil nil 0 comparator nil)))
+
+(defn emty [this]
+  (if (emty? this)
+    this
+    (.-nada this)))
+
+(defn cmpr [this x]
+  (.compare (.-comparator this) x (.getKey (.-t2 this))))
+
+(defn cntr [this]
+  (if (emty? this)
+    0
+    (.-cnt this)))
+
+(defn right-node [this]
+  (if (emty? (.-right this))
+    (emty this)
+    (.-right this)))
+
+(defn- left-node [this]
+  (if (emty? (.-left this))
+    (emty this)
+    (.-left this)))
